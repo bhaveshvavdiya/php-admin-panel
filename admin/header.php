d12d62a3e85f3a9864ae75df1edf1767746e6310
Bhavesh Vavadiya <bhaveshvavadiya89@gmail.com>
2018-07-30 16:11:27 +0530

    Common header file for css and js

---
+++ admin/header.php
@@ -0,0 +1,11 @@
+
+
+<!-- Bootstrap Core CSS -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    
+
+<!-- Bootstrap Datatable -->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css">
+
+<!-- Icon fonts -->
+<link href="./assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
